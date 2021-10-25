ab8269ac61413595001dce26a58fcc9550e5e234
David Ismail <69373282+sahabatdavid@users.noreply.github.com>
2021-10-25 04:56:39 -0700

    Create calcCircleArea.php

---
+++ php/calcCircleArea.php
@@ -0,0 +1,14 @@
+<?php
+
+class Calculate {
+  public function calcCircleArea($r) {
+    $phi = 3.14;
+    $circleArea = $phi * ($r ** 2);
+    return $rectangleArea;
+  }
+}
+
+$tri = new calcCircleArea;
+echo $tri->calcCircleArea(5,3);
+
+?>
